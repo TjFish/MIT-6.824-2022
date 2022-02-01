a997782d1d83d27b80f62aeb79f71e8f18b6461e
Cel A. Skeggs <cela@mit.edu>
2022-02-01 17:47:03 -0500

    update

--- src/main/test-mr.sh
+++ src/main/test-mr.sh
@@ -198,13 +198,13 @@ $TIMEOUT ../mrworker ../../mrapps/jobcount.so &
 $TIMEOUT ../mrworker ../../mrapps/jobcount.so
 
 NT=`cat mr-out* | awk '{print $2}'`
-if [ "$NT" -ne "8" ]
+if [ "$NT" -eq "8" ]
 then
+  echo '---' job count test: PASS
+else
   echo '---' map jobs ran incorrect number of times "($NT != 8)"
   echo '---' job count test: FAIL
   failed_any=1
-else
-  echo '---' job count test: PASS
 fi
 
 wait
